c7ff63f594d7f6de9c7185654c923eac28adfe10
Ozone <56304837+OzoneAsai@users.noreply.github.com>
2025-01-07 23:33:14 +0900

    Update port num

--- app.py
+++ app.py
@@ -253,4 +253,4 @@ def get_sound():
 # -----------------------------
 if __name__ == "__main__":
     # 開発時に使うポートなどを設定
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=7860, debug=True)
